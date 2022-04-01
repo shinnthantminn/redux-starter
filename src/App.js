bf11dabf1c51ff7a9e22e2d97f1e680af53cc270
shinnthantminn <85192394+shinnthantminn@users.noreply.github.com>
2022-04-01 17:53:46 +0630

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useId, useReducer, useRef } from "react";
+
+const initialState = [];
+
+function reducers(state, action) {
+  switch (action.type) {
+    case "Add":
+      return (state = [...state, action.payload]);
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducers, initialState);
+  const textRef = useRef();
+  const id = useId();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      id,
+      text: textRef.current.value,
+    };
+    dispatch({
+      type: "Add",
+      payload: data,
+    });
+    textRef.current.value = "";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "500px", margin: "0 auto" }}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="text">ToDo</label>
+        <br />
+        <input type="text" id="text" ref={textRef} autoComplete="off" />
+        <input type="submit" value="Add" />
+      </form>
+      <div>
+        {state.map((i) => (
+          <p key={i.id}>{i.text}</p>
+        ))}
+      </div>
     </div>
   );
 }
